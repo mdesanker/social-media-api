30b2cdfc239878ce95297f49224138fc3a758f6c
Michael Desanker <mdesanker@gmail.com>
2022-03-26 11:09:42 -0400

    Add login/logout routes

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -4,8 +4,18 @@ import passport from "passport";
 const CLIENT_URL =
   (process.env.CLIENT_URL as string) || (process.env.CLIENT_URL_DEV as string);
 
-const test = (req: Request, res: Response) => {
-  res.json({ msg: "Test route" });
+const loginSuccess = (req: Request, res: Response) => {
+  if (req.user) {
+    return res.status(200).json({
+      user: req.user,
+    });
+  } else {
+    return res.status(401).json({ msg: "Authentication failed" });
+  }
+};
+
+const loginFailed = (req: Request, res: Response) => {
+  res.status(401).json({ msg: "Authentication failed" });
 };
 
 const google = passport.authenticate("google", { scope: ["profile"] });
@@ -13,8 +23,13 @@ const google = passport.authenticate("google", { scope: ["profile"] });
 const googleCallback = [
   passport.authenticate("google", { failureRedirect: "/login/failed" }),
   (req: Request, res: Response) => {
-    res.redirect("/login/success");
+    res.redirect(CLIENT_URL);
   },
 ];
 
-export default { test, google, googleCallback };
+const logout = (req: Request, res: Response) => {
+  req.logout();
+  res.redirect(CLIENT_URL);
+};
+
+export default { loginSuccess, loginFailed, google, googleCallback, logout };
--- src/routes/api/auth.ts
+++ src/routes/api/auth.ts
@@ -3,9 +3,12 @@ import authController from "../../controllers/auth";
 
 const auth = Router();
 
-auth.get("/", authController.test);
+auth.get("/login/success", authController.loginSuccess);
+auth.get("/login/failed", authController.loginFailed);
 
 auth.get("/google", authController.google);
 auth.get("/google/callback", authController.googleCallback);
+
+auth.post("/logout", authController.logout);
 
 export = auth;
