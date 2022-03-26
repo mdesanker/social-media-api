a77dd6291e1925fe64ddcad94b205c88c23517e5
Michael Desanker <mdesanker@gmail.com>
2022-03-26 07:10:55 -0400

    Setup passport

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import cors from "cors";
 import helmet from "helmet";
 import compression from "compression";
 import connectDB from "./config/mongoConfig";
+import session from "express-session";
+import passport from "passport";
 
 import authRouter from "./routes/api/auth";
 
@@ -21,6 +23,19 @@ app.use(helmet());
 app.use(compression());
 app.use(express.json());
 app.use(urlencoded({ extended: false }));
+
+app.use(
+  session({
+    secret: process.env.SESSION_KEY as string,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 6,
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/auth", authRouter);
 
---
+++ src/strategies/googleStrategy.ts
@@ -0,0 +1,14 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import "dotenv/config";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      callbackURL: process.env.GOOGLE_CALLBACK as string,
+    },
+    async (accessToken, refreshToken, profile, cb) => {}
+  )
+);
